a9d50ed3e126822f4f45cbcf9fb0abdba89904d6
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-05-31 09:26:20 +0100

    Driver Classes

---
+++ Selenium-Testng-Template/src/test/kotlin/com/tech/selenium/driverutil/DriverFactory.kt
@@ -0,0 +1,51 @@
+package com.tech.selenium.driverutil
+
+import io.github.bonigarcia.wdm.WebDriverManager
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.edge.EdgeDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.ie.InternetExplorerDriver
+import org.openqa.selenium.opera.OperaDriver
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created by Sridhar Bandi on 31/05/19.
+ */
+object DriverFactory {
+
+    val browser: WebDriver
+        get() {
+
+            var driver: WebDriver
+            val browserName = System.getProperty("browser", DriverType.CHROME.toString()).toUpperCase()
+            val driverType = DriverType.valueOf(browserName)
+
+            when (driverType) {
+                DriverType.CHROME -> {
+                    WebDriverManager.chromedriver().setup()
+                    driver = ChromeDriver()
+                }
+                DriverType.FIREFOX -> {
+                    WebDriverManager.firefoxdriver().setup()
+                    driver = FirefoxDriver()
+                }
+                DriverType.EDGE -> {
+                    WebDriverManager.edgedriver().setup()
+                    driver = EdgeDriver()
+                }
+                DriverType.IE -> {
+                    WebDriverManager.iedriver().setup()
+                    driver = InternetExplorerDriver()
+                }
+                DriverType.OPERA -> {
+                    WebDriverManager.operadriver().setup()
+                    driver = OperaDriver()
+                }
+            }
+
+            driver.manage().window().maximize()
+            driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS)
+            return driver
+        }
+}

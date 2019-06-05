ddfa8c95f1281301119b322511f8c74256d39245
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-06-05 18:15:53 +0100

    Steps

---
+++ Selenium-Serenity-Cucumber-Template/src/test/kotlin/com/tech/selenium/steps/SearchSteps.kt
@@ -0,0 +1,30 @@
+package com.tech.selenium.steps
+
+import com.tech.selenium.pageobjects.GoogleSearchPage
+import com.tech.selenium.pageobjects.SearchResultsPage
+import net.thucydides.core.annotations.Step
+import net.thucydides.core.steps.ScenarioSteps
+
+/**
+ * Created by Sridhar Bandi on 05/06/19.
+ */
+open class SearchSteps : ScenarioSteps() {
+
+    internal var googleSearchPage: GoogleSearchPage? = null
+    internal var searchResultsPage: SearchResultsPage? = null
+
+    @Step
+    open fun openURL() {
+        googleSearchPage!!.open()
+    }
+
+    @Step
+    open fun searchFor(searchterm: String) {
+        googleSearchPage!!.searchFor(searchterm)
+    }
+
+    @Step
+    open fun seleniumLinkPresent(): Boolean {
+        return searchResultsPage!!.seleniumResultPresent()
+    }
+}

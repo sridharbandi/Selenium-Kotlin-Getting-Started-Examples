d1740cbeffcb291f8d82b80d53cde05d16fafa7c
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-06-06 19:45:30 +0100

    Page Objects

---
+++ Selenium-Jbehave-Junit-Template/src/test/kotlin/com/tech/selenium/pageobjects/GoogleSearchPage.kt
@@ -0,0 +1,31 @@
+package com.tech.selenium.pageobjects
+
+import org.openqa.selenium.Keys
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.support.FindBy
+import org.openqa.selenium.support.PageFactory
+
+/**
+ * Created by Sridhar Bandi on 06/06/19.
+ */
+class GoogleSearchPage(driver: WebDriver) {
+
+    private var driver: WebDriver? = null
+
+    @FindBy(name = "q")
+    internal var searchBox: WebElement? = null
+
+    init {
+        this.driver = driver
+        PageFactory.initElements(driver, this)
+    }
+
+    fun openURL() {
+        driver!!.get("https://www.google.com/")
+    }
+
+    fun searchFor(_searchTerm: String) {
+        searchBox!!.sendKeys(_searchTerm + Keys.RETURN)
+    }
+}

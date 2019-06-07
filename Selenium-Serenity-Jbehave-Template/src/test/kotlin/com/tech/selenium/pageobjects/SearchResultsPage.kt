2ab121c844bf4d81557595323c309aef779e1e0b
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-06-07 09:58:46 +0100

    Search Results Page Object

---
+++ Selenium-Serenity-Jbehave-Template/src/test/kotlin/com/tech/selenium/pageobjects/SearchResultsPage.kt
@@ -0,0 +1,21 @@
+package com.tech.selenium.pageobjects
+
+import net.serenitybdd.core.annotations.findby.FindBy
+import net.serenitybdd.core.pages.WebElementFacade
+import net.thucydides.core.pages.PageObject
+import org.openqa.selenium.WebDriver
+
+/**
+ * Created by Sridhar Bandi on 07/06/19.
+ */
+class SearchResultsPage : PageObject() {
+
+    private val _driver: WebDriver? = null
+
+    @FindBy(partialLinkText = "Selenium - Web Browser Automation")
+    internal var lnkSelenium: WebElementFacade? = null
+
+    fun seleniumResultPresent(): Boolean {
+        return lnkSelenium!!.isDisplayed
+    }
+}

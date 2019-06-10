74fc4ab1cb5df2eea27792f6970ec13d8afa9030
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-06-10 20:01:36 +0100

    Driver Factory Class

---
+++ Selenium-Selenide-Testng-Template/src/test/kotlin/com/tech/selenium/driverutil/DriverFactory.kt
@@ -0,0 +1,24 @@
+package com.tech.selenium.driverutil
+
+import com.codeborne.selenide.Configuration
+
+/**
+ * Created by Sridhar Bandi on 10/06/19.
+ */
+object DriverFactory {
+    fun setBrowser() {
+        val browserName = System.getProperty("browser", DriverType.CHROME.toString()).toUpperCase()
+        val _driverType = DriverType.valueOf(browserName)
+
+        when (_driverType) {
+            DriverType.CHROME -> System.setProperty("selenide.browser", "chrome")
+            DriverType.FIREFOX -> System.setProperty("selenide.browser", "firefox")
+            DriverType.EDGE -> System.setProperty("selenide.browser", "edge")
+            DriverType.IE -> System.setProperty("selenide.browser", "ie")
+            DriverType.OPERA -> System.setProperty("selenide.browser", "opera")
+            else -> System.setProperty("selenide.browser", "chrome")
+        }
+        Configuration.startMaximized = true
+        Configuration.timeout = 60000
+    }
+}

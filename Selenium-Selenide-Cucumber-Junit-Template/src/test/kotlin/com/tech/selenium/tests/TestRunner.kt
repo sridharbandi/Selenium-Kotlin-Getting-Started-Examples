0ca41bb1aee367e196587a88fb5ab30194ca306b
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-06-11 20:40:01 +0100

    Test Runner

---
+++ Selenium-Selenide-Cucumber-Junit-Template/src/test/kotlin/com/tech/selenium/tests/TestRunner.kt
@@ -0,0 +1,18 @@
+package com.tech.selenium.tests
+
+import cucumber.api.CucumberOptions
+import cucumber.api.junit.Cucumber
+import org.junit.runner.RunWith
+
+
+/**
+ * Created by Sridhar Bandi on 11/06/19.
+ */
+@RunWith(Cucumber::class)
+@CucumberOptions(
+    features = arrayOf("classpath:features/Google.feature"),
+    glue = arrayOf("com.tech.selenium.stepdefinitions"),
+    plugin = arrayOf("json:target/cucumber-report.json", "html:target/cucumber-html")
+)
+class TestRunner{
+}

fec908ab85c176f4a724fa26e1fb58605aa44131
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-06-19 20:55:21 +0100

    Google Search page

---
+++ Selenium-Selenide-Jbehave-Junit-Template/src/test/kotlin/com/tech/selenium/pageobjects/GoogleSearchPage.kt
@@ -0,0 +1,20 @@
+package com.tech.selenium.pageobjects
+
+import com.codeborne.selenide.Selectors.byName
+import com.codeborne.selenide.Selenide.`$`
+import com.codeborne.selenide.Selenide.open
+
+/**
+ * Created by Sridhar Bandi on 19/06/19.
+ */
+class GoogleSearchPage {
+    private val searchBox = `$`(byName("q"))
+
+    fun openURL() {
+        open("https://www.google.com/")
+    }
+
+    fun searchFor(searchTerm: String) {
+        searchBox.`val`(searchTerm).pressEnter()
+    }
+}

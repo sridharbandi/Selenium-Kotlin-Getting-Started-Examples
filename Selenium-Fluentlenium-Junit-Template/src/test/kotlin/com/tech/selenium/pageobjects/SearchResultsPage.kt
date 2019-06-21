0956240073e3f13a7e8e0f56d28161ba6d53397f
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-06-21 08:38:31 +0100

    Search Results Page

---
+++ Selenium-Fluentlenium-Junit-Template/src/test/kotlin/com/tech/selenium/pageobjects/SearchResultsPage.kt
@@ -0,0 +1,22 @@
+package com.tech.selenium.pageobjects
+
+import org.fluentlenium.core.FluentPage
+import org.fluentlenium.core.domain.FluentWebElement
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.support.FindBy
+import org.openqa.selenium.support.PageFactory
+
+/**
+ * Created by Sridhar Bandi on 21/06/19.
+ */
+class SearchResultsPage : FluentPage() {
+
+    @FindBy(partialLinkText = "Selenium - Web Browser Automation")
+    internal var lnkSelenium: FluentWebElement? = null
+
+    fun getLnkSelenium(): FluentWebElement? {
+        await().untilPage().isLoaded
+        return lnkSelenium!!
+    }
+}

63d323f726d28bc77a5c86e8a0ab4e594b1016c0
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-06-21 08:38:20 +0100

    Google Search Page

---
+++ Selenium-Fluentlenium-Junit-Template/src/test/kotlin/com/tech/selenium/pageobjects/GoogleSearchPage.kt
@@ -0,0 +1,27 @@
+package com.tech.selenium.pageobjects
+
+import org.fluentlenium.core.FluentPage
+import org.fluentlenium.core.domain.FluentWebElement
+import org.openqa.selenium.Keys
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.support.FindBy
+import org.openqa.selenium.support.PageFactory
+
+/**
+ * Created by Sridhar Bandi on 21/06/19.
+ */
+class GoogleSearchPage : FluentPage() {
+
+    @FindBy(name = "q")
+    internal var searchBox: FluentWebElement? = null
+
+    fun openURL() {
+        goTo("https://www.google.com/")
+    }
+
+    fun searchFor(searchTerm: String) {
+        searchBox!!.fill().with(searchTerm)
+        searchBox!!.submit()
+    }
+}
